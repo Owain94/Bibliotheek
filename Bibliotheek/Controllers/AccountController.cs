d5307ed176496afd8bc3e1957bb7d983a9cc44e3
Owain van Brakel <Owain.vanbrakel@gmail.com>
2014-09-15 14:50:27 +0200

    Debug values

--- Bibliotheek/Controllers/AccountController.cs
+++ Bibliotheek/Controllers/AccountController.cs
@@ -15,7 +15,7 @@ namespace Bibliotheek.Controllers
         #region Public Methods
 
         //
-        // GET: /Account/Register 
+        // GET: /Account/Register
         [EnableCompression]
         public ActionResult Activate()
         {
@@ -42,6 +42,46 @@ namespace Bibliotheek.Controllers
             }
 
             model.GetValues(token);
+
+            return View(model);
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [EnableCompression]
+        public ActionResult Activate(ActivateModel model)
+        {
+            var token = string.Empty;
+            try
+            {
+                token = SqlInjection.SafeSqlLiteral(Url.RequestContext.RouteData.Values["id"].ToString());
+            }
+            // ReSharper disable EmptyGeneralCatchClause 
+            catch (Exception)
+            // ReSharper restore EmptyGeneralCatchClause 
+            {
+                Response.Redirect("http://66164.ict-lab.nl/", true);
+            }
+
+            if (String.IsNullOrEmpty(token) || token.Length != 32)
+            {
+                Response.Redirect("http://66164.ict-lab.nl/", true);
+            }
+
+            model.GetValues(token);
+
+            Response.Write("ID: " + model.Id + "<br />");
+            Response.Write("Voornaam: " + model.Firstname + "<br />");
+            Response.Write("Tussenvoegsel: " + model.Inclusion + "<br />");
+            Response.Write("Achternaam: " + model.Lastname + "<br />");
+            Response.Write("Email: " + model.Mail + "<br />");
+            Response.Write("Postcode: " + Crypt.StringEncrypt(SqlInjection.SafeSqlLiteral(StringManipulation.ToUpperFast(model.PostalCode)).Replace(" ", string.Empty), model.Pepper) + "<br />");
+            Response.Write("Huisnummer: " + Crypt.StringEncrypt(SqlInjection.SafeSqlLiteral(model.HouseNumber), model.Pepper) + "<br />");
+            Response.Write("Geslacht: " + model.Gender + "<br />");
+            Response.Write("Geboortedatum: " + StringManipulation.DateTimeToMySql(model.DOB) + "<br />");
+            Response.Write("Wachtwoord: " + Crypt.StringEncrypt(SqlInjection.SafeSqlLiteral(model.Password), model.Pepper) + "<br />");
+            Response.Write("Pepper: " + model.Pepper + "<br />");
 
             return View(model);
         }
